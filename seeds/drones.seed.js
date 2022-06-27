32e4997298e1af4f3f43cb572e923b0d91d9dc99
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-06-27 19:38:18 +0200

    model y seed listos

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,29 @@
 // Iteration #1
+const dronesArr = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  // direccion de la seed a mano en Drones
+  const Drones = require("../models/Drone.model")
+
+  //coneccion a la DB
+
+  const mongoose = require("mongoose")
+
+  mongoose
+  .connect("mongodb://localhost:27017/drone")
+  .then((response) => {
+    console.log("BD arriba");
+
+    return Drones.insertMany(dronesArr);
+  })
+  .then((response) => {
+    console.log("drones insertados en la DB");
+
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
